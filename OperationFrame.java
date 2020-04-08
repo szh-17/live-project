f66f6dbfd5c05a50c68ea1fd94ff73d3733eab5d
szh <60462369+szh-17@users.noreply.github.com>
2020-04-08 21:00:52 +0800

    Update OperationFrame.java

--- OperationFrame.java
+++ OperationFrame.java
@@ -6,8 +6,9 @@ import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+//记录离校信息
 public class OperationFrame extends JFrame implements ActionListener {
-   
+    /************************定义各控件******************************/
     private String welcomMsg="选择如下操作:";
     private JLabel lblWelcome=new JLabel(welcomMsg);
     private JButton btQuery=new JButton("显示详细信息");
@@ -24,8 +25,8 @@ public class OperationFrame extends JFrame implements ActionListener {
         this.setSize(300,250);
         GUIUtil.toCenter(this);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setResizable(false);
-        this.setVisible(true);
+        this.setResizable(false); //窗口大小不改变
+        this.setVisible(true);//将指定对象设为可见
         /**********************增加监听*******************************/
         btQuery.addActionListener(this);
         btModify.addActionListener(this);
@@ -34,7 +35,7 @@ public class OperationFrame extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource()==btQuery) {
-            String message="详细资料为:\n";
+            String message="您的详细资料为:\n";
             message+="姓名："+Conf.account+"\n";
             message+="学号："+Conf.password+"\n";
             message+="入校时间："+Conf.time+"\n";
